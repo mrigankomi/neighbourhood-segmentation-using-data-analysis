50523c27da824c4e9940dee8a89b241611d1808c
mrigankomi <68615865+mrigankomi@users.noreply.github.com>
2021-02-01 10:32:24 -0600

    Add files via upload

---
+++ notebook_capstone.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h3>This is my capstone project final notebook</h3>
+
+# This notebook will be used to create a project for neighbourhood segmentation in vancouver using crime statistics data foursquare API
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# <h1>Hello coursera capstone!</h1>
+
+# In[ ]:
+
+
+
+
